2c3b66d4aee4b3dc481cba998282fd6c24942c1b
DAW <DAW@ILERNA.SEVILLA>
2023-01-27 12:39:34 +0100

    add sql

---
+++ example-app/school.sql
@@ -0,0 +1,11 @@
+INSERT INTO students (`name`,surname,age,birthdate,course,`avg`,genre,email) VALUES('Mario','Ariza',19,'2003-10-06',2,8.75,'M','mariochulo69@gmail.com');
+INSERT INTO students (`name`,surname,age,birthdate,course,`avg`,genre,email) VALUES('Javi','Antequera',20,'2002-12-31',2,9.5,'M','javiantequera@gmail.com');
+INSERT INTO students (`name`,surname,age,birthdate,course,`avg`,genre,email) VALUES('Manuel','Moyazo',19,'2003-12-10',2,10,'M','moyarechulon69@gmail.com');
+INSERT INTO students (`name`,surname,age,birthdate,course,`avg`,genre,email) VALUES('Fran','Pikabros',20,'2003-01-28',2,2.5,'M','pikabrosfolleti@gmail.com');
+INSERT INTO students (`name`,surname,age,birthdate,course,`avg`,genre,email) VALUES('Jose','Gym',19,'2002-02-19',2,4.8,'M','joseproteinas@gmail.com');
+
+INSERT INTO subjects (`name`,course,difficulty,hours,credits) VALUES('Programacion','1','h',230,5);
+INSERT INTO subjects (`name`,course,difficulty,hours,credits) VALUES('Acceso a datos','2','h',210,4);
+INSERT INTO subjects (`name`,course,difficulty,hours,credits) VALUES('Entornos de desarrollo','1','l',140,3);
+INSERT INTO subjects (`name`,course,difficulty,hours,credits) VALUES('Lenguaje de marcas','1','m',250,4);
+INSERT INTO subjects (`name`,course,difficulty,hours,credits) VALUES('Desarrollo de interfaces','2','m',280,5);
